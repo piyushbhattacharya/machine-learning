060728732168328f949c4c99815e42e4fdd522a4
Piyush Bhattacharya <piyushbhattacharya@gmail.com>
2017-07-27 12:27:20 +0530

    Update titanic_lr.scala

--- scala/titanic_lr.scala
+++ scala/titanic_lr.scala
@@ -1,3 +1,7 @@
+/**
+* Classification using Logistic Regression
+**/
+
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.log4j._
